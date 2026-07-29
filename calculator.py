55882c4b30ee5ded92d7c9c1a915a71280882cd1
Rob Rogers <rrob31070@gmail.com>
2026-07-29 13:01:04 -0400

    fixed calc

--- calculator.py
+++ calculator.py
@@ -20,20 +20,22 @@ def square_root(a):
     try:
         return math.sqrt(a)
     except ValueError:
-        print("num must be greater than 0")
+        raise ValueError
 
 def hypotenuse(a, b):
     return math.hypot(a,b)
+
+def exp(a,b):
+    return a ** b
 
 def subtract(a, b):
     return a - b
 
 def logarithm(a, b):
     try:
-        math.log(b, a)
+        return math.log(b, a)
     except ValueError:
-        print("Value Error")
-
+        raise ValueError
 
 
 
